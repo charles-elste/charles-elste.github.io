8eb1eb5cfc9712c99092b1a8e782d314b31ac9c2
charles-elste <58530961+charles-elste@users.noreply.github.com>
2020-11-08 22:40:05 -0800

    Add files via upload

---
+++ elstecap/main.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: .25, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.25 });
+tl.to(".intro", { y: "-100%", duration: 0.25 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.25 });
+tl.fromTo(".nav__links", { opacity: 0 }, { opacity: 1, duration: 0.25 }, "-=1");
+
+// Based on animation code by Dev Ed youtube.com/watch?v=sN93DRYkCO8&t=1310s
